b139529c9b3e9cdc9703103b57474f38d623db23
georgw97 <georg.wollert@outlook.com>
2021-10-30 17:51:21 +0200

    Game v1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,52 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // create the Windo
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "My Window");
 
-    while (window.isOpen())
+    // create the texture
+    sf::Texture texture;
+    // it shall be a 200x100 texture, empty
+    if (!texture.loadFromFile("testtexture.png", sf::IntRect(0, 0 , 16 , 160)))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        // error...
     }
 
+    // create the texutre
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setPosition(sf::Vector2f(10.f, 50.f));
+
+    //Game variables
+    float playerMoveSpeed = 10;
+    //game loop
+    while(window.isOpen()){
+
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while(window.pollEvent(event)){
+            // "close requested" event: we close the window
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed){
+                if (event.key.code == sf::Keyboard::S){
+                    sprite.move(sf::Vector2f(0.f, playerMoveSpeed));
+                }
+                if (event.key.code == sf::Keyboard::W){
+                    sprite.move(sf::Vector2f(0.f, playerMoveSpeed));
+                }
+            }
+        }
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+        // draw everything here
+        window.draw(sprite);
+
+        //to here
+        window.display();
+    }   
     return 0;
 }
