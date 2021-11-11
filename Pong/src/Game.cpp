967d6390178026ee3e6e0a055c73c3881cc3c5d4
georgw97 <georg.wollert@outlook.com>
2021-11-11 20:19:02 +0100

    Fix sphagetti code v1

---
+++ Pong/src/Game.cpp
@@ -0,0 +1,9 @@
+
+class Game
+{
+public:
+	float playerMoveSpeed; // speed
+	float playerMoveY; // how much the player moves (specified in loop)
+	float playerHeight;
+	float playerWidth;
+};
--- Pong/src/main.cpp
+++ Pong/src/main.cpp
@@ -2,48 +2,76 @@
 #include <iostream>
 #include "time.hpp"
 
-static bool initialize() {
+static sf::RenderWindow window(sf::VideoMode(1280, 720), "Pong");
+static sf::Clock sclock;
+static sf::Clock collisionClock;
+static sf::Font font;
+static sf::Texture playertexture;
 
-}
-
-int main()
+class Game
 {
-	//game res
+private:
+
+
+public:
+	// Game Variables 
+	//Graphics
 	int resHor = 1280;
 	int resVer = 720;
 
-	//Game variables
-	float playerMoveSpeed = 1000.0; // speed
-	float playerMoveY = 0.0; // how much the player moves (specified in loop)
+	// Game state
+	int points = 0;
+	int life = 0;
+
+	// Player Variables
+	float playerMoveSpeed; // speed at which the Player tavels
+	float playerMoveY; // how much the player moves (specified in loop)
 	float playerHeight = 100.0;
 	float playerWidth = 10.0;
+	//
+	bool gameOn = false;
+};
+
+Game game;
+
+bool Initialize()
+{
+
+
+	if(!font.loadFromFile("../fonts/CascadiaMono.ttf"))
+	{
+		return -1;
+	}
+
+	if(!playertexture.loadFromFile("../textures/testtexture.png", sf::IntRect(0, 0, game.playerWidth, game.playerHeight)))
+	{
+		return -1;
+	}
+}
+
+void MainMenu()
+{
+
+}
+
+
+
+void RunGame()
+{
+	//check if initialized and running
+
+
+	//Game variables
+	game.playerMoveSpeed = 1000.0; // speed
+	game.playerMoveY = 0.0; // how much the player moves (specified in loop)
+
 
 	//initial movement of ball
 	float ballMovementSpeedX = 1000.0;
 	srand(time(0));
 	float ballMovementSpeedY = 50 - (rand() % 100);
 
-	//pointcounter
-	int points = 0;
-	int life = 0;
-
-	//game on or not
-	bool gameOn = false;
-
-	//create the text font
-	sf::Font font;
-	if (!font.loadFromFile("../fonts/CascadiaMono.ttf")) {
-		return 2;
-	}
-
-	//ceate the text
-	sf::Text pointstext;
-	pointstext.setFont(font);
-	pointstext.setString(std::to_string(points));
-	pointstext.setCharacterSize(24);
-	pointstext.setFillColor(sf::Color::White);
-	pointstext.setPosition(sf::Vector2f(resHor * 0.5, 0 + 10));
-
+	// Init the main Menu
 	sf::Text mainMenuText;
 	mainMenuText.setFont(font);
 	mainMenuText.setString("WELCOME To Pong Game -- Made by Georg Wollert\n\n"
@@ -56,127 +84,143 @@ int main()
 	mainMenuText.setPosition(sf::Vector2f(10, 10));
 	mainMenuText.setOrigin(sf::Vector2f());
 
+	// Init the text
+	sf::Text pointstext;
+	pointstext.setFont(font);
+	pointstext.setString(std::to_string(game.points));
+	pointstext.setCharacterSize(24);
+	pointstext.setFillColor(sf::Color::White);
+	pointstext.setPosition(sf::Vector2f(game.resHor * 0.5, 0 + 10));
 
-	// initialize the clock
-	sf::Clock sclock;
-	sf::Clock collisionClock;
-
-	// initialize the Windo
-	sf::RenderWindow window(sf::VideoMode(resHor, resVer), "My Window");
-
-	// create the texture
-	sf::Texture playertexture;
-	// it shall be a 16x1600 texture, empty
-	if (!playertexture.loadFromFile("../textures/testtexture.png", sf::IntRect(0, 0, playerWidth, playerHeight)))
-	{
-		// error...
-		return 1;
-	}
 	// create the player
 	sf::Sprite player1;
 	player1.setTexture(playertexture);
-	player1.setPosition(sf::Vector2f(10.f, resVer / 2));
-	player1.setOrigin(5, playerHeight / 2.0);
+	player1.setPosition(sf::Vector2f(10.f, game.resVer / 2));
+	player1.setOrigin(5, game.playerHeight / 2.0);
 
 	//create the ball
 	sf::CircleShape ball(10.f);
 	ball.setFillColor(sf::Color(250, 250, 250));
-	ball.setPosition(sf::Vector2f(resHor / 2, resVer / 2));
+	ball.setPosition(sf::Vector2f(game.resHor / 2, game.resVer / 2));
 	ball.setOrigin(5, 5);
 
+	//Start loop
 	//game loop
-	while (window.isOpen()) {
+	while(window.isOpen())
+	{
 
 		// check all the window's events that were triggered since the last iteration of the loop
 		sf::Event event;
-		while (window.pollEvent(event)) {
+		while(window.pollEvent(event))
+		{
 			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed) {
+			if(event.type == sf::Event::Closed)
+			{
 				window.close();
 			}
 			// Game 1
 			// key pressed keybinds
-			if (event.type == sf::Event::KeyPressed) {
+			if(event.type == sf::Event::KeyPressed)
+			{
 				//Main menu Logic
-				if (!gameOn) {
-					if (event.key.code == sf::Keyboard::Num1) {
-						points = 0;
-						pointstext.setString(std::to_string(points));
-						ball.setPosition(sf::Vector2f(resHor / 2, resVer / 2));
+				if(!game.gameOn)
+				{
+					if(event.key.code == sf::Keyboard::Num1)
+					{
+						game.points = 0;
+						pointstext.setString(std::to_string(game.points));
+						ball.setPosition(sf::Vector2f(game.resHor / 2, game.resVer / 2));
 						ballMovementSpeedX = 1000;
 						ballMovementSpeedY = 50 - (rand() % 100);
-						gameOn = true;
+						game.gameOn = true;
 					}
-					else if (event.key.code == sf::Keyboard::Num2) {
+					else if(event.key.code == sf::Keyboard::Num2)
+					{
 
 					}
-					else if (event.key.code == sf::Keyboard::Num3) {
+					else if(event.key.code == sf::Keyboard::Num3)
+					{
 						//Disp highscore from database
 					}
-					else if (event.key.code == sf::Keyboard::Num4) {
+					else if(event.key.code == sf::Keyboard::Num4)
+					{
 						window.close();
 					}
-					else if (event.key.code == sf::Keyboard::Escape) {
+					else if(event.key.code == sf::Keyboard::Escape)
+					{
 						window.close();
 					}
 				}
 				//Game Logic
-				if (gameOn) {
-					if (event.key.code == sf::Keyboard::S) {
-						playerMoveY = playerMoveSpeed;
+				if(game.gameOn)
+				{
+					if(event.key.code == sf::Keyboard::S)
+					{
+						game.playerMoveY = game.playerMoveSpeed;
 					}
-					else if (event.key.code == sf::Keyboard::W) {
-						playerMoveY = -playerMoveSpeed;
+					else if(event.key.code == sf::Keyboard::W)
+					{
+						game.playerMoveY = -game.playerMoveSpeed;
 					}
-					else if (event.key.code == sf::Keyboard::Escape) {
-						gameOn = false;
+					else if(event.key.code == sf::Keyboard::Escape)
+					{
+						game.gameOn = false;
 					}
 
 				}
 			}
-			if (event.type == sf::Event::KeyReleased) {
-				if (event.key.code == sf::Keyboard::S) {
-					playerMoveY = 0;
+			if(event.type == sf::Event::KeyReleased)
+			{
+				if(event.key.code == sf::Keyboard::S)
+				{
+					game.playerMoveY = 0;
 				}
-				else if (event.key.code == sf::Keyboard::W) {
-					playerMoveY = 0;
+				else if(event.key.code == sf::Keyboard::W)
+				{
+					game.playerMoveY = 0;
 				}
 			}
 		}
 		// Player Out of Scope handling
-		if (player1.getPosition().y > resVer - playerHeight / 2.0) {
-			player1.setPosition(sf::Vector2f(10, resVer - playerHeight / 2.0));
+		if(player1.getPosition().y > game.resVer - game.playerHeight / 2.0)
+		{
+			player1.setPosition(sf::Vector2f(10, game.resVer - game.playerHeight / 2.0));
 		}
-		else if (player1.getPosition().y <= 0 + playerHeight / 2.0) {
-			player1.setPosition(sf::Vector2f(10, playerHeight / 2.0));
+		else if(player1.getPosition().y <= 0 + game.playerHeight / 2.0)
+		{
+			player1.setPosition(sf::Vector2f(10, game.playerHeight / 2.0));
 		}
 
 		// Ball bouncing handling
-		if (ball.getPosition().x >= 1280) {
+		if(ball.getPosition().x >= 1280)
+		{
 			ballMovementSpeedX = -ballMovementSpeedX;
 		}
-		else if (ball.getPosition().y >= resVer - 15 || (ball.getPosition().y <= 0)) {
+		else if(ball.getPosition().y >= game.resVer - 15 || (ball.getPosition().y <= 0))
+		{
 			ballMovementSpeedY = -ballMovementSpeedY;
 		}
 		// player missed ball handling
-		else if (ball.getPosition().x <= 0) {
+		else if(ball.getPosition().x <= 0)
+		{
 			//Restart
-			points = 0;
-			pointstext.setString(std::to_string(points));
-			ball.setPosition(sf::Vector2f(resHor / 2, resVer / 2));
+			game.points = 0;
+			pointstext.setString(std::to_string(game.points));
+			ball.setPosition(sf::Vector2f(game.resHor / 2, game.resVer / 2));
 			ballMovementSpeedX = 1000;
 			ballMovementSpeedY = 50 - (rand() % 100);
 		}
 
 		//collision handling
-		if (ball.getGlobalBounds().intersects(player1.getGlobalBounds())) {
-
+		if(ball.getGlobalBounds().intersects(player1.getGlobalBounds()))
+		{
 			//check if 1 second has passed since last collision
-			if (collisionClock.getElapsedTime().asSeconds() > 1) {
+			if(collisionClock.getElapsedTime().asSeconds() > 1)
+			{
 				collisionClock.restart();
 				//increment points 
-				points++;
-				pointstext.setString(std::to_string(points));
+				game.points++;
+				pointstext.setString(std::to_string(game.points));
 				//check where the ball exactly intersects with the player
 				float ballPosY = ball.getPosition().y;
 				float playerPosY = player1.getPosition().y;
@@ -191,7 +235,7 @@ int main()
 		}
 
 		// player and ball need to move
-		player1.move(sf::Vector2f(0, playerMoveY * deltaT(sclock)));
+		player1.move(sf::Vector2f(0, game.playerMoveY * deltaT(sclock)));
 		ball.move(sf::Vector2f(ballMovementSpeedX * deltaT(sclock), ballMovementSpeedY * deltaT(sclock)));
 
 		// restart clock
@@ -200,17 +244,31 @@ int main()
 		// clear the window with black color
 		window.clear(sf::Color::Black);
 		// draw everything here
-		if (gameOn) {
+		if(game.gameOn)
+		{
 			window.draw(ball);
 			window.draw(player1);
 			window.draw(pointstext);
 		}
-		else {
+		else
+		{
 			window.draw(mainMenuText);
 		}
 
 		//to here
 		window.display();
 	}
+
+}
+
+
+
+int main()
+{
+	Initialize();
+	MainMenu();
+	RunGame();
+
+	
 	return 0;
 }
