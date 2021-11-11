fc0df5273ec345f72f382e8fc908e6ffeb3c3f62
georgw97 <georg.wollert@outlook.com>
2021-11-11 19:11:49 +0100

    VS Redo

--- Makefile
+++
@@ -1,6 +0,0 @@
-all: compile link
-
-compile:
-	g++ -I src/include -c main.cpp -c funcs.cpp
-link:
-	g++ main.o funcs.o -o main -L src/lib -l sfml-graphics -l sfml-window -l sfml-system
---
+++ Pong/src/time.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+
+float deltaT(sf::Clock clock);
+
+void restartGame(int& points, int& ballMovementSpeedX, int& ballMovementSpeedY, int& resHor, int& resVer, sf::Text& pointstext, sf::CircleShape& ball);
+
